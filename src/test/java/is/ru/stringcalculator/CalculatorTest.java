04745d403b7d4669a2ec1b9da3d9ce530e1face4
AdamHaukur <adamb14@ru.is>
2015-10-14 20:33:08 +0000

    Test for oneNumber

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,4 +13,9 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(4, Calculator.add("4"));
+	}
 }
