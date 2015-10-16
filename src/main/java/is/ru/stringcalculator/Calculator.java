6e78d72e7981990359424bd8cf7215bbfc1b7726
AdamHaukur <adamb14@ru.is>
2015-10-16 11:12:47 +0000

    code for Delimiter Number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,26 +1,34 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
+	public static String del = ",|\n|;";
 
 	public static int add(String text){
+		String[] numbers = text.split(del);
+
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",") | text.contains("\n")){
-			return sum(splitNumbers(text));
-		}
-		else
+		else if(text.length() == 1){
 			return toInt(text);
+		}
+		else if(text.startsWith("//")){
+			return sum(delimiter(text));
+		}
+		else{
+			return sum(numbers);
+		}
 	}
 
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",|\n");
+	private static String[] delimiter(String numbers) {
+		numbers = numbers.substring(4, numbers.length());
+		return numbers.split(del);
 	}
-      
+	
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
